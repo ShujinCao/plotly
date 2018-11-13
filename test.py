d6f93556b90cea5306328a7020a147a4afb725ff
Jackie Cao <jackcao@uw.edu>
2018-11-12 20:27:38 -0800

    for lightning talk

--- test.py
+++ test.py
@@ -1,79 +1,42 @@
 #!/usr/bin/env python3
 
-import plotly.plotly as py
-import plotly.graph_objs as go
+import plotly.plotly as pp
+import plotly.graph_objs as pg
+import pandas as pd
 
-# Add data
+# read in mortality data
+data = pd.read_csv("out.csv")
+# select age < 1 data
+data = data[data.age_group_name == '<1 year']
+# select the columns we need
+data = data[['fit','year_id','sex']]
 
-month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-         'August', 'September', 'October', 'November', 'December']
-high_2000 = [32.5, 37.6, 49.9, 53.0, 69.1, 75.4, 76.5, 76.6, 70.7, 60.6, 45.1, 29.3]
-low_2000 = [13.8, 22.3, 32.5, 37.2, 49.9, 56.1, 57.7, 58.3, 51.2, 42.8, 31.6, 15.9]
-high_2007 = [36.5, 26.6, 43.6, 52.3, 71.5, 81.4, 80.5, 82.2, 76.0, 67.3, 46.1, 35.0]
-low_2007 = [23.6, 14.0, 27.0, 36.8, 47.6, 57.7, 58.9, 61.2, 53.3, 48.5, 31.0, 23.6]
-high_2014 = [28.8, 28.5, 37.0, 56.8, 69.7, 79.7, 78.5, 77.8, 74.1, 62.6, 45.3, 39.9]
-low_2014 = [12.7, 14.3, 18.6, 35.5, 49.9, 58.0, 60.0, 58.6, 51.7, 45.2, 32.2, 29.1]
+# prepare input data for graphing
+male_data = pg.Scatter(
+    x = data['year_id'][data['sex'] == 'Male'],
+    y = data['fit'][data['sex'] == 'Male'],
+    name = 'Male',
+    mode ='markers',
+    marker = dict(
+        color = ('rgb(205, 12, 24)'))
+)
 
-# Create and style traces
-trace0 = go.Scatter(
-    x = month,
-    y = high_2014,
-    name = 'High 2014',
-    line = dict(
-        color = ('rgb(205, 12, 24)'),
-        width = 4)
+female_data = pg.Scatter(
+    x = data['year_id'][data['sex'] == 'Female'],
+    y = data['fit'][data['sex'] == 'Female'],
+    name = 'Female',
+    mode ='markers',
+    marker = dict(
+        color = ('rgb(12, 12, 24)'))
 )
-trace1 = go.Scatter(
-    x = month,
-    y = low_2014,
-    name = 'Low 2014',
-    line = dict(
-        color = ('rgb(22, 96, 167)'),
-        width = 4,)
-)
-trace2 = go.Scatter(
-    x = month,
-    y = high_2007,
-    name = 'High 2007',
-    line = dict(
-        color = ('rgb(205, 12, 24)'),
-        width = 4,
-        dash = 'dash') # dash options include 'dash', 'dot', and 'dashdot'
-)
-trace3 = go.Scatter(
-    x = month,
-    y = low_2007,
-    name = 'Low 2007',
-    line = dict(
-        color = ('rgb(22, 96, 167)'),
-        width = 4,
-        dash = 'dash')
-)
-trace4 = go.Scatter(
-    x = month,
-    y = high_2000,
-    name = 'High 2000',
-    line = dict(
-        color = ('rgb(205, 12, 24)'),
-        width = 4,
-        dash = 'dot')
-)
-trace5 = go.Scatter(
-    x = month,
-    y = low_2000,
-    name = 'Low 2000',
-    line = dict(
-        color = ('rgb(22, 96, 167)'),
-        width = 4,
-        dash = 'dot')
-)
-data = [trace0, trace1, trace2, trace3, trace4, trace5]
+
+data_both = [male_data, female_data]
 
 # Edit the layout
-layout = dict(title = 'Average High and Low Temperatures in New York',
-              xaxis = dict(title = 'Month'),
-              yaxis = dict(title = 'Temperature (degrees F)'),
+layout = dict(title = 'Mortality Rate 1980 - 2040',
+              xaxis = dict(title = 'Year'),
+              yaxis = dict(title = 'Rate')
               )
 
-fig = dict(data=data, layout=layout)
-py.iplot(fig, filename='styled-line')
+fig = dict(data=data_both, layout=layout)
+pp.iplot(fig, filename='line', auto_open=True)
